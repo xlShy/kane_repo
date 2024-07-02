4aacbd1df90688a97d3bd5400159bddbc890a1c4
John Vincent Espinosa <johnvincentespinosa1@gmail.com>
2024-07-02 11:00:23 +0800

    Implemented Dialogue Queueing System - JV

--- Assets/Scripts/DialogueScript/DialogueTriggerScript.cs
+++ Assets/Scripts/DialogueScript/DialogueTriggerScript.cs
@@ -18,18 +18,23 @@ public class DialogueTriggerScript : MonoBehaviour
     private bool isRepeating;
 
     private bool alreadyActivated = false;
-    private Coroutine dialogueCoroutine;
+    private static Queue<DialogueInfo> dialogueQueue = new Queue<DialogueInfo>();
+    private static Coroutine dialogueCoroutine;
     private int currentDialogueIndex = 0;
+
+    private class DialogueInfo
+    {
+        public List<string> Content;
+        public float Duration;
+        public bool IsRepeating;
+        public DialogueTriggerScript Trigger;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!alreadyActivated && dialogueContent.Count > 0)
         {
-            if (dialogueCoroutine != null)
-            {
-                StopCoroutine(dialogueCoroutine);
-            }
-            dialogueCoroutine = StartCoroutine(DisplayAllDialogues());
+            EnqueueDialogue();
         }
     }
 
@@ -41,7 +46,35 @@ public class DialogueTriggerScript : MonoBehaviour
         }
     }
 
-    private IEnumerator DisplayAllDialogues()
+    private void EnqueueDialogue()
+    {
+        DialogueInfo info = new DialogueInfo
+        {
+            Content = new List<string>(dialogueContent),
+            Duration = dialogueDuration,
+            IsRepeating = isRepeating,
+            Trigger = this
+        };
+
+        dialogueQueue.Enqueue(info);
+
+        if (dialogueCoroutine == null)
+        {
+            dialogueCoroutine = StartCoroutine(ProcessDialogueQueue());
+        }
+    }
+
+    private static IEnumerator ProcessDialogueQueue()
+    {
+        while (dialogueQueue.Count > 0)
+        {
+            DialogueInfo currentDialogue = dialogueQueue.Dequeue();
+            yield return currentDialogue.Trigger.DisplayAllDialogues(currentDialogue);
+        }
+        dialogueCoroutine = null;
+    }
+
+    private IEnumerator DisplayAllDialogues(DialogueInfo info)
     {
         for (int i = 0; i < dialogueContent.Count; i++)
         {
