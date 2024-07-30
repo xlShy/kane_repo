3132756593b011f0f992a74a26c8ef4ea4997756
PatrickJohn243 <florespatrickjohn243@gmail.com>
2024-07-30 17:11:11 +0800

    CHANGED UI TO INSPECT FOR READABLE DOCUMENTS - Patrick

---
+++ Assets/Scripts/QuestObjectivesScripts/Doors/GateClosing.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateClosing : MonoBehaviour
+{
+    public DoorBase doorBase;
+
+    private void Start()
+    {
+        //doorBase.LockDoor();
+    }
+}
